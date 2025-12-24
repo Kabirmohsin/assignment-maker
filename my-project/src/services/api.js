21b02f5d3d9828711505fc6464bd0c1e151f4c4e
Kabir Mohsin <kabirmohsin892@gmail.com>
2025-12-24 15:51:58 +0530

    Update api.js

--- my-project/src/services/api.js
+++ my-project/src/services/api.js
@@ -7,11 +7,12 @@ import axios from "axios";
 */
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://assignment-maker-backend.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
 });
+;
 
 /* ===============================
    🔐 ATTACH JWT TOKEN
